f6cb718f9d8599f4474763da2255092928f3c27e
agerms <159292017+agerms@users.noreply.github.com>
2024-02-14 11:28:26 -0500

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTkn0qaX5lzuoTWGAsQA_r0M7tlrZpl2fFGm5qklse3I5vWvc9QybHcrKFwBWbpouCmvz-63BPYFiXm/pubhtml';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1J3mTRHugVkDL3Gx6XAz7E-KhSuXgoC8ksjw53Hfl8U4/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
